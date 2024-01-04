adb39bb8fffdcc218ba2c3c421dca80233a7b916
Yuvaraj S R <yuvaraj.ramesh@aravind.org>
2024-01-04 13:34:51 +0530

    Controller update - Audio Config
    Signed-off-by: Yuvaraj S R <yuvaraj.ramesh@aravind.org>

--- Controllers/TranscriptionController.cs
+++ Controllers/TranscriptionController.cs
@@ -1,8 +1,14 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Protobuf;
+using NAudio;
 using Google.Cloud.Speech.V1;
 using Grpc.Core;
 using Microsoft.AspNetCore.Mvc;
+using Grpc.Auth;
+using System.IO;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace TranscriptionAPI.Controllers
 {
@@ -17,18 +23,30 @@ namespace TranscriptionAPI.Controllers
                 string? environmentVariablePath = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
                               
                 GoogleCredential credentials = GoogleCredential.FromFile(environmentVariablePath);
-                SpeechClientBuilder builder = new SpeechClientBuilder { CredentialsPath = environmentVariablePath };
+                //SpeechClientBuilder builder = new SpeechClientBuilder { CredentialsPath = environmentVariablePath };
+                SpeechClientBuilder builder = new SpeechClientBuilder { ChannelCredentials = credentials.ToChannelCredentials() };
                 SpeechClient speech = await builder.BuildAsync();
 
                 RecognitionConfig config = new RecognitionConfig
                 {
                     Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
                     SampleRateHertz = 16000,
-                    LanguageCode = LanguageCodes.English.UnitedStates
+                    LanguageCode = LanguageCodes.English.UnitedStates,
+                    AudioChannelCount = 1, // Set the number of channels in the audio (if applicable)
+                    EnableSeparateRecognitionPerChannel = false, // Enable recognition for each audio channel separately
+                    MaxAlternatives = 5, // Set the maximum number of recognition alternatives to return
+                    ProfanityFilter = true, // Enable or disable profanity filtering
+                    EnableWordTimeOffsets = true, // Enable word time offsets in the response
+                    EnableWordConfidence = true, // Enable word confidence in the response
+                    EnableAutomaticPunctuation = true, // Enable automatic punctuation detection
+                    EnableSpokenPunctuation = true, // Enable spoken punctuation
+                    EnableSpokenEmojis = true, // Enable spoken emojis
+                    UseEnhanced = true// Use enhanced models for speech recognition
                 };
 
                 RecognitionAudio audio = RecognitionAudio.FromBytes(audioData);
                 RecognizeResponse response = await speech.RecognizeAsync(config, audio);
+               // Console.WriteLine($"Response received: {response}");
 
                 string transcript = "";
 
@@ -64,10 +82,12 @@ namespace TranscriptionAPI.Controllers
             }
             catch (RpcException ex)
             {
+                //Console.WriteLine("gRPC Error: " + ex);
                 return "Error transcribing audio: " + ex.Status.Detail;
             }
             catch (Exception ex)
             {
+               // Console.WriteLine("Error: " + ex);
                 return "Error transcribing audio: " + ex.Message;
             }
        
